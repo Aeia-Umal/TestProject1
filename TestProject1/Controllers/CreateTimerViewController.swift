dcbe9e78ecfb6a136e7b4f41d9042737e7f5814a
Aeia <aeia@Joenels-MacBook-Pro-2.local>
2020-09-22 15:46:58 +0800

    Initial Commit

---
+++ TestProject1/Controllers/CreateTimerViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CreateTimerVC.swift
+//  TestProject1
+//
+//  Created by Aeia on 8/18/20.
+//  Copyright © 2020 n/a. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class CreateTimerViewController: UIViewController {
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    var timePickerVC = TimerPickerViewController()
+    
+    var timer = Timer()
+    
+    
+    @IBOutlet weak var firstBTN: UIButton!
+    @IBOutlet weak var secondBTN: UIButton!
+    @IBOutlet weak var thirdBTN: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+//        if segue.identifier == Segues.toTimerPickerVC {
+//
+//        }
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+    
+    @IBAction func didTapFirstBTN(_ sender: UIButton) {
+        timePickerVC.cancelTimer()
+        // FirstBTN = done/cancel button
+    }
+    
+    @IBAction func didTapSecondBTN(_ sender: UIButton) {
+        // SecondBTN = start/pause/resume button
+    }
+    
+    @IBAction func didTapThirdBTN(_ sender: UIButton) {
+        // ThirdBTN = reset button
+    }
+}
---
+++ TestProject1/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  TestProject1
+//
+//  Created by Aeia on 9/22/20.
+//  Copyright © 2020 n/a. All rights reserved.
+//
+
+//import AVFoundation
+//import CircleProgressView
+//import SwipeCellKit
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func createTimerBTN(_ sender: UIButton) {
+        
+        
+    }
+    
+
+    @IBAction func savedTimersBTN(_ sender: UIButton) {
+    }
+    
+    @IBAction func savedAudioBTN(_ sender: UIButton) {
+    }
+    
+    @IBAction func settingsBTN(_ sender: UIButton) {
+    }
+    
+}
+
